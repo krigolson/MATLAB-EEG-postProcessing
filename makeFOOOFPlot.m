6ec402e2a69937ee47b09ea21b21084f34a34ca0
Olav Krigolson <44245195+krigolson@users.noreply.github.com>
2024-07-19 10:18:02 -0700

    newPlots

--- makeFOOOFPlot.m
+++ makeFOOOFPlot.m
@@ -1,4 +1,4 @@
-function  makeFOOOFPlot(aperiodicData,powerData,task,channel,fooofFolder)
+function  makeFOOOFPlot(aperiodicData,powerData,task,channel,peak,fooofFolder,legendText)
 
 %Aperiodic data is a participant-meaned matrix of aperiodic series data
 %for a given channel and task with the dimensions of frequency x condition. 
@@ -7,26 +7,39 @@ numConditions = size(aperiodicData,2);
 f= figure;
 f.WindowState = 'maximized';
 
-title(['FOOOF results for ' task ' Task - ' channel])
 hold on
 
-colours(:,1) = {'black'};
-colours(:,2) = {'blue'};
-colours(:,3) = {'red'};
-colours(:,4) = {'green'};
+colours(:,1) = [0.69 0.27 0.96];
+colours(:,2) = [0.18 0.44 0.82];
+colours(:,3) = [0.11 0.69 0.42];
+colours(:,4) = [0.91 0.11 0.32];
 
+minSpot = min(min(powerData));
+maxSpot = max(max(powerData));
 
 for condition = 1:numConditions
-    a(condition) = plot(aperiodicData(:,condition),'Color',colours{condition},'LineWidth',2);
+    a(condition) = plot(aperiodicData(:,condition),'Color',colours(:,condition),'LineWidth',2);
     hold on
 
-    b(condition) = plot(powerData(:,condition),'Color',colours{condition},'LineStyle','--','LineWidth',2);
+    b(condition) = plot(powerData(:,condition),'Color',colours(:,condition),'LineStyle','--','LineWidth',2);
     hold on
 
 end
 
-legend([a,b(1)],{'Aperiodic Fit - No Shirt','Aperiodic Fit - Long Sleeve','Aperiodic Fit - Short Sleeve','Aperiodic Fit - Experimental','Power Spectra'})
-legend boxoff
+xlim([1,30])
+
+rectangle('Position',[peak(1)-peak(3),0,peak(3)*2,maxSpot-minSpot],'FaceColor',[0.72 0.72 0.72 0.25],'EdgeColor','none');
+yline(0,'Color', [0 0 0],'LineWidth',1.5)
+
+s=gca;
+s.XAxis.TickLength = [0 0];
+s.XAxis.FontSize = 25;
+s.YAxis.TickLength = [0 0];
+s.YAxis.FontSize = 25;
+
+l = legend([a,b(1)],legendText);
+l.Box = "off";
+fontsize(l,20,"points");
 
 ylabel('Power')
 xlabel('Frequency')
